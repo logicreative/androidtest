d2996623d17c5ac9c19d51256238b946c8e0412f
Lukas Welte <lukas.welte@googlemail.com>
2013-11-17 17:30:05 +0100

    androidtest-7 test failing request

--- SampleApp/src/instrumentTest/java/com/adm/app/test/MainActivityTest.java
+++ SampleApp/src/instrumentTest/java/com/adm/app/test/MainActivityTest.java
@@ -40,4 +40,8 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     public void testSuccess() {
         assertEquals(true, true);
     }
+
+    public void testFail() {
+        assertEquals(1 + 1, 3);
+    }
 }
