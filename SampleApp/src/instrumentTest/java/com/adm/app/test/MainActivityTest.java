29f92b6f28db9dc0a4308fd437cba3eb102e2de0
Lukas Welte <lukas.welte@googlemail.com>
2013-11-17 12:51:42 +0100

    init

---
+++ SampleApp/src/instrumentTest/java/com/adm/app/test/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.adm.app.test;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ActivityTestCase;
+
+import com.adm.app.MainActivity;
+
+/**
+ * Created by lukas on 17.11.13.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mTestActivity;
+
+    @TargetApi(Build.VERSION_CODES.FROYO)
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mTestActivity = getActivity();
+
+    }
+
+    public void testPreconditions() {
+        assertNotNull("mTestActivity is null", mTestActivity);
+    }
+
+    public void testCalc() {
+        assertEquals(3+3,6);
+    }
+}
